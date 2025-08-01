ce4aaafad0dd2ac2433af376bf15415a696c82d5
Luca Cocchia <lucacocchia1234@gmail.com>
2025-07-31 23:52:02 -0500

    added some stuff to vehicles

--- src/shared/vehicles.ts
+++ src/shared/vehicles.ts
@@ -26,7 +26,44 @@ class Aircraft {
     }
 }
 
-const AC130J = new Aircraft("AC130J Gunship", 299, 164000, 13830, ["105mm", "30mm", "7.62mm", "AGM-176", "AGM-114", "GBU-39"], 70000, 3130, true, ["105mm M102 Howitzer", "30mm GAU-23/A", "7.62mm GAU-21"], 6);
+const AC130J = new Aircraft(
+    "AC130J Gunship",
+    299,
+    164000,
+    13830,
+    ["105mm", "30mm", "7.62mm", "AGM-176", "AGM-114", "GBU-39"],
+    70000,
+    3130,
+    true,
+    ["105mm M102 Howitzer", "30mm GAU-23/A", "7.62mm GAU-21"],
+    6
+);
 
-const vehicles = { AC130J };
+const F16C = new Aircraft(
+    "F-16C Viper",
+    1500, // max speed in knots approx Mach 2
+    42000, // max takeoff weight (lbs)
+    17000, // weapon load limit (lbs)
+    ["AIM-9", "AIM-120", "GBU-12", "AGM-65", "Mk-82"],
+    19000, // base weight (lbs)
+    8200, // min runway length (ft)
+    true,
+    ["M61 Vulcan 20mm", "AIM-9 Sidewinder", "AIM-120 AMRAAM"],
+    1
+);
+
+const A10C = new Aircraft(
+    "A-10C II",
+    450, // max speed (knots)
+    29000, // mtow (lbs)
+    16500, // weapon load limit (lbs)
+    ["GAU-8/A", "AGM-65", "Hydra 70", "Mk-82", "CBU-87"],
+    17000, // base weight (lbs)
+    5200, // min runway length (ft)
+    true,
+    ["GAU-8/A Avenger 30mm", "AGM-65 Maverick", "Hydra 70 Rockets"],
+    1
+);
+
+const vehicles = { AC130J, F16C, A10C };
 export default vehicles;
